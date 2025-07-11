d2881a60a28002f9ca1fd9129c5a79f1349abee9
MochiMor <jose.villarreal@qmmcmx.com>
2025-07-11 17:40:47 -0600

    feat(cs): added screen transition script

---
+++ Assets/Scripts/MapTransition.cs
@@ -0,0 +1,54 @@
+using Cinemachine;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class MapTransition : MonoBehaviour
+{
+
+    [SerializeField] PolygonCollider2D mapBoundry;
+    CinemachineConfiner confiner;
+    [SerializeField] Direction direction;
+    [SerializeField] float additivePos;
+    enum Direction {Up, Down, Left, Right}
+
+    private void Awake()
+    {
+        confiner = FindAnyObjectByType<CinemachineConfiner>();
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            confiner.m_BoundingShape2D = mapBoundry;
+            UpdatePlayerPosition(collision.gameObject);
+        }
+    }
+
+    private void UpdatePlayerPosition(GameObject player)
+    {
+        Vector3 newPos = player.transform.position;
+
+        switch (direction)
+        {
+            case Direction.Up:
+                newPos.y += additivePos;
+                break;
+            case Direction.Down:
+                newPos.y -= additivePos;
+                break;
+            case Direction.Left:
+                newPos.x += additivePos;
+                break;
+            case Direction.Right:
+                newPos.y -= additivePos;
+                break;
+
+        }
+
+    player.transform.position = newPos;
+    }
+
+}
