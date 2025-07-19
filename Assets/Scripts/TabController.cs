d5a546041963cdbcb6b0adec834b1d13078051b1
MochiMor <jose.villarreal@qmmcmx.com>
2025-07-19 13:44:41 -0600

    feat(c#): Added controller for the menu tabs

---
+++ Assets/Scripts/TabController.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabController : MonoBehaviour
+{
+    public Image[] tabImages;
+    public GameObject[] pages;
+    public GameObject menuCanvas;
+    public int activeTab = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ActivateTab(activeTab);
+    }
+
+    private void Update()
+    {
+
+        if (menuCanvas != null && menuCanvas.activeSelf) {
+
+            if (Input.GetKeyDown(KeyCode.Q)) { PreviousTab(); }
+
+            if (Input.GetKeyDown(KeyCode.E)) { NextTab(); }
+
+        }
+
+    }
+
+
+    public void ActivateTab(int tabNo)
+    {
+        for (int i = 0; i < tabImages.Length; i++) {
+            pages[i].SetActive(false);
+            tabImages[i].color = Color.grey;
+        }
+
+        pages[tabNo].SetActive(true);
+        tabImages[tabNo].color = Color.white;
+
+        activeTab = tabNo;
+
+    }
+
+    public void NextTab()
+    {
+        int next = (activeTab + 1) % tabImages.Length;
+        ActivateTab(next);
+    }
+
+    public void PreviousTab()
+    {
+        int prev = (activeTab - 1 + tabImages.Length) % tabImages.Length;
+        ActivateTab(prev);
+    }
+}
