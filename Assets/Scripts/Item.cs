50896a767a5aa45075b00d123bab5aecbac4e84c
MochiMor <jose.villarreal@qmmcmx.com>
2025-09-11 11:41:11 -0600

    feat(cs): created item class

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+
+    public int ID;
+    public string Name;
+
+    public virtual void Pickup()
+    {
+        Sprite itemIcon = GetComponent<Image>().sprite;
+        if(ItemPickupUIController.Instance != null)
+        {
+            ItemPickupUIController.Instance.ShowItemPickup(Name, itemIcon);
+        }
+    }
+
+    public virtual void UseItem()
+    {
+        Debug.Log($"Using item: {Name}");
+    }
+}
