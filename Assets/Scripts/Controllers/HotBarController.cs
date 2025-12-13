8cda4e04cdddf36ae37621063615e839575d920d
JoseHuVill <136835243+JoseHuVill@users.noreply.github.com>
2025-12-13 14:35:07 -0600

    fix(cs): dont use items if menu is open

--- Assets/Scripts/Controllers/HotBarController.cs
+++ Assets/Scripts/Controllers/HotBarController.cs
@@ -32,7 +32,7 @@ public class HotBarController : MonoBehaviour
     {
         for (int i = 0; i < slotCount; i++)
         {
-            if (Keyboard.current[hotbarKeys[i]].wasPressedThisFrame)
+            if (Keyboard.current[hotbarKeys[i]].wasPressedThisFrame && !PauseController.IsGamePaused)
             {
                 //UseItem
                 UseItemInSlot(i);
