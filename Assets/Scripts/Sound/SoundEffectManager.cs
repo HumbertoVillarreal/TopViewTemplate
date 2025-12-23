5d8e6a269bb6f0eb91c8db38892d1215a1a4224b
JoseHuVill <136835243+JoseHuVill@users.noreply.github.com>
2025-12-23 09:19:22 -0600

    fix(cs): gave style and more functionality to volume functions

--- Assets/Scripts/Sound/SoundEffectManager.cs
+++ Assets/Scripts/Sound/SoundEffectManager.cs
@@ -1,4 +1,6 @@
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class SoundEffectManager : MonoBehaviour
@@ -9,6 +11,8 @@ public class SoundEffectManager : MonoBehaviour
     private static AudioSource voiceAudioSource;
     private static SoundEffectLibrary soundEffectLibrary;
     [SerializeField] private Slider sfxSlider;
+    [SerializeField] private GameObject[] soundBars;
+
 
     private void Awake()
     {
@@ -48,6 +52,7 @@ public class SoundEffectManager : MonoBehaviour
     void Start()
     {
         sfxSlider.onValueChanged.AddListener(delegate { OnValueChanged(); });
+        OnValueChanged();
     }
 
     public static void SetVolume(float volume)
@@ -56,6 +61,11 @@ public class SoundEffectManager : MonoBehaviour
         randomPitchAudioSource.volume = volume;
         voiceAudioSource.volume = volume;
         BgMusicManager.instance.getAudioSource().volume = volume;
+
+        //Round for bars
+        float rounded = Mathf.Clamp(Mathf.Round((volume * 10) / 2f) * 2f, 0f, 10f);
+        SetBars(rounded);
+        //Debug.Log("Volume: "+ volume);
     }
 
     public void OnValueChanged()
@@ -63,10 +73,37 @@ public class SoundEffectManager : MonoBehaviour
         SetVolume(sfxSlider.value);
     }
 
+    public static void SetBars(float rounded)
+    {
+   
+        int filledBars = (int)(rounded / 2f);
+
+
+        for (int i = 0; i < Instance.soundBars.Length; i++)
+        {
+            Image img = Instance.soundBars[i].GetComponent<Image>();
+
+            if (i < filledBars)
+                img.color = Color.green;   // filled
+            else
+                img.color = Color.white;    // empty
+        }
+    }
+
 
     public static void PlayVoice(AudioClip audioClip, float pitch = 1f)
     {
         voiceAudioSource.pitch = pitch;
         voiceAudioSource.PlayOneShot(audioClip);
+    }
+
+    public void IncreaseVolume()
+    {
+        sfxSlider.value = Mathf.Clamp(sfxSlider.value + .2f, 0f, 1f);
+    }
+
+    public void DecreaseVolume()
+    {
+        sfxSlider.value = Mathf.Clamp(sfxSlider.value - .2f, 0f, 1f);
     }
 }
